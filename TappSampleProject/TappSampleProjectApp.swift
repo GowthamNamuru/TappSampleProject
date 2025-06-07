0d6417f0c1b7ca3b908e9ca6c4a6a2a0b4b88800
gowthamnamuru <gowthamnamuri@gmail.com>
2025-06-07 06:08:05 +0530

    Add `VibeView`

--- TappSampleProject/TappSampleProjectApp.swift
+++ TappSampleProject/TappSampleProjectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TappSampleProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VibeView()
         }
     }
 }
---
+++ TappSampleProject/VibeView.swift
@@ -0,0 +1,18 @@
+//
+//  VibeView.swift
+//  TappSampleProject
+//
+//  Created by Gowtham Namuru on 07/06/25.
+//
+
+import SwiftUI
+
+struct VibeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    VibeView()
+}
