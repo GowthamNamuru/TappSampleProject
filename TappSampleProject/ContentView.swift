e1a532f713e977ea8a8c1b5a7cc10cdd9555632b
gowthamnamuru <gowthamnamuri@gmail.com>
2025-06-07 06:01:24 +0530

    Initial Commit

---
+++ TappSampleProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TappSampleProject
+//
+//  Created by Gowtham Namuru on 07/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TappSampleProject/TappSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TappSampleProjectApp.swift
+//  TappSampleProject
+//
+//  Created by Gowtham Namuru on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TappSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
